d7cd12e3921ca9a1ec8242785d4551fd671ddc22
Andranik Sahakyan <8908029+andranik-sahakyan@users.noreply.github.com>
2019-05-14 00:35:23 -0700

    Testing fix for terminating bg procs

--- include/helpers.h
+++ include/helpers.h
@@ -1,8 +1,5 @@
 int timeComparator(ProcessEntry_t* node1, ProcessEntry_t* node2);
 ProcessEntry_t* findByPid(List_t* bg_list, pid_t pid); 
 void printBgList(List_t* bg_list);
-<<<<<<< Updated upstream
 void killBgProcs(List_t* bg_list); 
-=======
->>>>>>> Stashed changes
 int configureIO(char* args[], size_t numTokens);
